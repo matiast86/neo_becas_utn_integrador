f4ca12ab2351d9e3cc3cb150b717d0bbdee7be96
Facundo Hernandez <facujhernandez46@gmail.com>
2023-10-24 23:46:40 -0300

    Creacion de la Clase Equipo y Funcion resultado (modificable)

---
+++ github.com/matiast86/neo_becas_utn_integrador.git/TrabsjoPracticoCurso/src/main/java/trabajoPracticoCurso/Grupo7/Equipo.java
@@ -0,0 +1,27 @@
+package trabajoPracticoCurso.Grupo7;
+
+public class Equipo {
+
+	
+	private String nombre;
+
+	
+	
+
+	
+	public Equipo(String nombre) {
+		
+		this.nombre=nombre;
+
+	}
+	
+	
+	
+	public String getNombre() {
+		return this.nombre;
+		
+	}
+
+	
+	
+}
