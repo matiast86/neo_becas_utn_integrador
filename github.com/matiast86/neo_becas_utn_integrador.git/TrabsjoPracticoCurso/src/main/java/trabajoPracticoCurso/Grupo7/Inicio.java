be0337af4a24c1bd73d4e4d3dc2788cc26d8c33f
Facundo Hernandez <facujhernandez46@gmail.com>
2023-11-02 23:17:46 -0300

    se comenzo el inicio donde se va a leer y crear los objetos del csv

--- github.com/matiast86/neo_becas_utn_integrador.git/TrabsjoPracticoCurso/src/main/java/trabajoPracticoCurso/Grupo7/Inicio.java
+++ github.com/matiast86/neo_becas_utn_integrador.git/TrabsjoPracticoCurso/src/main/java/trabajoPracticoCurso/Grupo7/Inicio.java
@@ -1,8 +1,20 @@
 package trabajoPracticoCurso.Grupo7;
 
+import java.util.ArrayList;
+
 public class Inicio {
 
 	public static void main(String[] args) {
+		//C:\Users\Pais Gamer\git\TrabajoCursoJava\github.com\matiast86\neo_becas_utn_integrador.git\TrabsjoPracticoCurso\src\main\archivos
+		
+		if(args.length==0) {
+			System.out.println("ERROR: No ingrestaste ningun archivo como argumento!");
+			System.exit(88);
+		}
+		
+		LectorDeArchivos lectorArchivos = new LectorDeArchivos(args[0]);
+		
+		ArrayList<Partido> partidos = lectorArchivos.listaPartidos();
 		
 	}
 	
