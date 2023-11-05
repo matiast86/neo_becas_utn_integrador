490a52e661734d8d3e5c1211cb6205c0eec82933
Facundo Hernandez <facujhernandez46@gmail.com>
2023-11-04 21:56:25 -0300

    Implementacion del LectorDeArchivo(Fallo de la nada pero funciona)

--- github.com/matiast86/neo_becas_utn_integrador.git/TrabsjoPracticoCurso/src/main/java/trabajoPracticoCurso/Grupo7/Inicio.java
+++ github.com/matiast86/neo_becas_utn_integrador.git/TrabsjoPracticoCurso/src/main/java/trabajoPracticoCurso/Grupo7/Inicio.java
@@ -2,6 +2,7 @@ package trabajoPracticoCurso.Grupo7;
 
 import java.util.ArrayList;
 
+
 public class Inicio {
 
 	public static void main(String[] args) {
@@ -11,11 +12,29 @@ public class Inicio {
 			System.out.println("ERROR: No ingrestaste ningun archivo como argumento!");
 			System.exit(88);
 		}
+		else  if (args.length==1){
+			System.out.println("ERROR: solo ingrestaste un archivo como argumento!");
+			System.exit(88);
+		}
 		
-		LectorDeArchivos lectorArchivos = new LectorDeArchivos(args[0]);
+		else{
+			LectorDeArchivos lectorArchivosResultado = new LectorDeArchivos(args[0]);
+			lectorArchivosResultado.parsearArchivoResultados();
+			
+			ArrayList<Partido> partidos = lectorArchivosResultado.listaPartidos();
+			LectorDeArchivos lectorArchivosPronostico = new LectorDeArchivos(args[1]);
+			lectorArchivosPronostico.parsearArchivoPronosticos();
+			
+			//elimino un partido que se crea sin valores en la ultima posicion del array
+			partidos.remove(partidos.size()-1);
+	 
+			ArrayList<Pronostico> pronosticos = lectorArchivosPronostico.listarPronosticos(partidos);		
+			
+			System.out.println(pronosticos.size());
+			
+
 		
-		ArrayList<Partido> partidos = lectorArchivos.listaPartidos();
-		
-	}
+		}
 	
+	}
 }
--- github.com/matiast86/neo_becas_utn_integrador.git/TrabsjoPracticoCurso/src/main/java/trabajoPracticoCurso/Grupo7/Partido.java
+++ github.com/matiast86/neo_becas_utn_integrador.git/TrabsjoPracticoCurso/src/main/java/trabajoPracticoCurso/Grupo7/Partido.java
@@ -1,5 +1,7 @@
 package trabajoPracticoCurso.Grupo7;
 
+import java.util.ArrayList;
+
 public class Partido {
 
 	private Equipo equipo1;
@@ -43,5 +45,46 @@ public class Partido {
 		}
 
 	}
+
+	
+	
+	
+	public Equipo getEquipo1() {
+		return equipo1;
+	}
+
+	public void setEquipo1(Equipo equipo1) {
+		this.equipo1 = equipo1;
+	}
+
+	public Equipo getEquipo2() {
+		return equipo2;
+	}
+
+	public void setEquipo2(Equipo equipo2) {
+		this.equipo2 = equipo2;
+	}
+
+	public int getGolesEquipo1() {
+		return golesEquipo1;
+	}
+
+	public void setGolesEquipo1(int golesEquipo1) {
+		this.golesEquipo1 = golesEquipo1;
+	}
+
+	public int getGolesEquipo2() {
+		return golesEquipo2;
+	}
+
+	public void setGolesEquipo2(int golesEquipo2) {
+		this.golesEquipo2 = golesEquipo2;
+	}
+	
+	
+	
+	
+	
+	
 }		
 		
