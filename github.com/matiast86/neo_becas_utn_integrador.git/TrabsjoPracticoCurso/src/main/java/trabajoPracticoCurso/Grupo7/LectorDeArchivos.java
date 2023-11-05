67a474c7acf2a9af810767299ebf3ac5c613748f
Facundo Hernandez <facujhernandez46@gmail.com>
2023-11-04 21:53:17 -0300

    Implementacion de funciones ListarPronosticos y listarPartidos

--- github.com/matiast86/neo_becas_utn_integrador.git/TrabsjoPracticoCurso/src/main/java/trabajoPracticoCurso/Grupo7/LectorDeArchivos.java
+++ github.com/matiast86/neo_becas_utn_integrador.git/TrabsjoPracticoCurso/src/main/java/trabajoPracticoCurso/Grupo7/LectorDeArchivos.java
@@ -48,14 +48,73 @@ public class LectorDeArchivos {
          
         }
     }
-	public ArrayList<Pronostico> listarPronosticos(){
-		return null;
+	public ArrayList<Pronostico> listarPronosticos(ArrayList<Partido> PartidosRealizados){
+		ArrayList<Pronostico> pronosticos = new ArrayList<Pronostico>();
+		
+		for (ArchivosPronosticos lineaArchivoPronostico : this.pronosticosList) {
+			 String chequeoEquipo1= lineaArchivoPronostico.getEquipo1();
+			 String chequeoEquipo2= lineaArchivoPronostico.getEquipo2();
+			Pronostico nuevoPronostico =null;
+			for(Partido p : PartidosRealizados){
+				 //chequeo
+				 System.out.println(chequeoEquipo1);
+				 System.out.println(chequeoEquipo2);
+				 //
+				 
+				 
+				if(p.getEquipo1().getNombre().equals(chequeoEquipo1) && p.getEquipo2().getNombre().equals(chequeoEquipo2)){
+					if(lineaArchivoPronostico.getGana1()=='X') {
+						nuevoPronostico = new Pronostico (p,p.getEquipo1(),resultadoEnum.Ganador);  
+					}
+					else if (lineaArchivoPronostico.getGana2()=='X') {
+						nuevoPronostico = new Pronostico (p,p.getEquipo2(),resultadoEnum.Ganador);  
+					}
+					else {
+						nuevoPronostico = new Pronostico (p,p.getEquipo1(),resultadoEnum.Empato);
+					}
+					
+				}
+				 
+				 
+			 }
+			
+			if(!pronosticos.contains(nuevoPronostico)) {
+				pronosticos.add(nuevoPronostico);
+				System.out.println("pronostico creado");
+			}
+			
+		
+		}
+		
+		
+		return pronosticos;
 		
 	}
-    
+
+
+   
 	
 	public ArrayList<Partido> listaPartidos(){
-		return null;
+		ArrayList<Partido> partidos = new ArrayList<Partido>();
+		
+		for(ArchivoResultados lineaArchivoResultado : this.resultadosList) {
+			Equipo nuevoEquipo1= new Equipo(lineaArchivoResultado.getEquipo1());
+			Equipo nuevoEquipo2= new Equipo(lineaArchivoResultado.getEquipo2());
+			
+			
+			
+			
+			Partido nuevoPartido= new Partido (nuevoEquipo1,nuevoEquipo2, lineaArchivoResultado.getGoles1(),lineaArchivoResultado.getGoles2());
+			
+			
+			if(!partidos.contains(nuevoPartido)) {
+				partidos.add(nuevoPartido);
+			}
+		}
+		
+		
+		
+		return partidos;
 	}
 		
 	
