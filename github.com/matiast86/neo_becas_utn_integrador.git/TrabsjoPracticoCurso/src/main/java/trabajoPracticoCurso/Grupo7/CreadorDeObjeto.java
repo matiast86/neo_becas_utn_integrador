dd82bf2f88da66a059f758848da0a5718e1efe7d
Facundo Hernandez <facujhernandez46@gmail.com>
2023-11-06 23:04:02 -0300

    Finalizacion de la creacion de objetos de Apostadores y Rondas(Falta chequear el de Apostadores)

--- github.com/matiast86/neo_becas_utn_integrador.git/TrabsjoPracticoCurso/src/main/java/trabajoPracticoCurso/Grupo7/CreadorDeObjeto.java
+++ github.com/matiast86/neo_becas_utn_integrador.git/TrabsjoPracticoCurso/src/main/java/trabajoPracticoCurso/Grupo7/CreadorDeObjeto.java
@@ -18,53 +18,72 @@ public class CreadorDeObjeto {
 	
 	
 	public ArrayList<Apostador> listarApostadores(ArrayList<Ronda> RondasRealizadas) {
-		ArrayList<Apostador> ApostadosCreados = new ArrayList<Apostador>();
+		ArrayList<Apostador> apostadoresCreados = new ArrayList<Apostador>();
 		ArrayList<Pronostico> pronosticos = new ArrayList<Pronostico>();
 
 		for (ArchivosPronosticos lineaArchivoPronostico : this.pronosticosList) {
+			Apostador apostadorSeleccionado;
+			
+			if(!Apostador.ApostadorEstaEnLista(lineaArchivoPronostico.getApostador(),apostadoresCreados)) {
+				apostadorSeleccionado= new Apostador(lineaArchivoPronostico.getApostador());
+				apostadoresCreados.add(apostadorSeleccionado);
+			}
+			
+			apostadorSeleccionado= Apostador.obtenerApostador(lineaArchivoPronostico.getApostador(),apostadoresCreados);
+			int chequeoRonda = lineaArchivoPronostico.getRonda();
 			String chequeoEquipo1 = lineaArchivoPronostico.getEquipo1();
 			String chequeoEquipo2 = lineaArchivoPronostico.getEquipo2();
 			Pronostico nuevoPronostico = null;
-			for (Partido p : PartidosRealizados) {
-
-				if (p.getEquipo1().getNombre().equals(chequeoEquipo1)
-						&& p.getEquipo2().getNombre().equals(chequeoEquipo2)) {
-					if (lineaArchivoPronostico.getGana1() == 'X') {
-						nuevoPronostico = new Pronostico(p, p.getEquipo1(), resultadoEnum.Ganador);
-					} else if (lineaArchivoPronostico.getGana2() == 'X') {
-						nuevoPronostico = new Pronostico(p, p.getEquipo2(), resultadoEnum.Ganador);
-					} else {
-						nuevoPronostico = new Pronostico(p, p.getEquipo1(), resultadoEnum.Empato);
+			
+			if(Ronda.estaEnLista(chequeoRonda, RondasRealizadas)) {
+				Ronda ronda = Ronda.obtenerRondaConNumero(chequeoRonda, RondasRealizadas);
+				for (Partido p : ronda.getPartidos()) {
+					
+					if (p.getEquipo1().getNombre().equals(chequeoEquipo1)
+							&& p.getEquipo2().getNombre().equals(chequeoEquipo2)) {
+						if (lineaArchivoPronostico.getGana1() == 'X') {
+							nuevoPronostico = new Pronostico(p, p.getEquipo1(), resultadoEnum.Ganador);
+						} else if (lineaArchivoPronostico.getGana2() == 'X') {
+							nuevoPronostico = new Pronostico(p, p.getEquipo2(), resultadoEnum.Ganador);
+						} else {
+							nuevoPronostico = new Pronostico(p, p.getEquipo1(), resultadoEnum.Empato);
+						}
+					
 					}
-
+			
 				}
 
 			}
+			
 			if (!pronosticos.contains(nuevoPronostico)) {
 				pronosticos.add(nuevoPronostico);
 			}
 
 		}
 
-		return ApostadosCreados;
+		return apostadoresCreados;
 
 	}
 
+
+
+
+
+
 	public ArrayList<Ronda> ListarRonda() {
 		ArrayList<Ronda> rondasCreadas = new ArrayList<Ronda>();
-		ArrayList<Partido> partidos = new ArrayList<Partido>();
-
 		for (ArchivoResultados lineaArchivoResultado : this.resultadosList) {
 			Ronda RondaCaptada;
-			if (!estaEnLista(lineaArchivoResultado.getRonda(), rondasCreadas)) {
+			if (!Ronda.estaEnLista(lineaArchivoResultado.getRonda(), rondasCreadas)) {
 				RondaCaptada = new Ronda(lineaArchivoResultado.getRonda());
 				rondasCreadas.add(RondaCaptada);
 			}
 
-			RondaCaptada = obtenerRondaConNumero(lineaArchivoResultado.getRonda(), rondasCreadas);
+			RondaCaptada = Ronda.obtenerRondaConNumero(lineaArchivoResultado.getRonda(), rondasCreadas);
 
 			Equipo nuevoEquipo1 = new Equipo(lineaArchivoResultado.getEquipo1());
 			Equipo nuevoEquipo2 = new Equipo(lineaArchivoResultado.getEquipo2());
+
 
 			Partido nuevoPartido = new Partido(nuevoEquipo1, nuevoEquipo2, lineaArchivoResultado.getGoles1(),
 					lineaArchivoResultado.getGoles2());
@@ -77,26 +96,12 @@ public class CreadorDeObjeto {
 		return rondasCreadas;
 	}
 
-	public static boolean estaEnLista(int numeroRonda, ArrayList<Ronda> rondas) {
-		for (Ronda r : rondas) {
-			if (r.getNro() == numeroRonda) {
-				return true;
-			}
-		}
-		return false;
-	}
 
-	public Ronda obtenerRondaConNumero(int numeroRonda, ArrayList<Ronda> rondas) {
-		for (Ronda r : rondas) {
-			if (r.getNro() == numeroRonda) {
-				return r;
-			}
-		}
-		return null;
 
-	}
+	
+
     
-    
-    
+
+	
     
 }
